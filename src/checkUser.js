395675dbbdf091ece3384f45eb16b7fabb97538c
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 18:04:37 -0500

    Update checkUser.js
    make it look like the demo with try/catch and log all user data

--- src/checkUser.js
+++ src/checkUser.js
@@ -1,24 +1,24 @@
 import { Auth } from 'aws-amplify'; // allows getting user info from cognito
 
 const checkUser = async(updateUser) => {
-    const userData = await Auth
-        .currentSession() // checking to see if we have data for this user
-        .catch(err => console.log('You got a problem here: ', err)
-        )
-    if (!userData) {
-        console.log('someUser data: ', userData) // log user data to console if none?
-        updateUser({})
-        return // returns if there is no user data
+    try {
+        const userData = await Auth
+            .currentSession() // checking to see if we have data for this user
+        console.log("check, check user 1, 2... ", userData)
+       
+        // nested destructuring below - idToken has payload about user
+        const { idToken: { payload }} = userData
+        // is this user in the Admin group? is it in any group?
+        const isAuthorized = payload['cognito:groups'] &&
+            payload['cognito:groups'].includes('Admin')
+        updateUser({
+            username: payload['cognito:username'],
+            isAuthorized // if user is Admin, isAuthorized is set to true
+        })
+    } catch (err) {
+        console.error(err);
     }
-    // nested destructuring below - idToken has payload about user
-    const { idToken: { payload }} = userData
-    // is this user in the Admin group? is it in any group?
-    const isAuthorized = payload['cognito:groups'] &&
-        payload['cognito:groups'].includes('Admin')
-    updateUser({
-        username: payload['cognito:username'],
-        isAuthorized // if user is Admin, isAuthorized is set to true
-    })
+    
 }
 
 export default checkUser;
