98d024b87b53fe1762b59b5be6925bd6df9338cf
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-27 23:00:39 -0500

    Update index.js
    put in the imports
    Note: installing react-router-dom put in the new syntax my errors kept throwing at me
    Router from ./Router will be useful later...

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Router from './Router';
+import 'antd/dist/reset.css';
+import Amplify from 'aws-amplify';
+import config from './aws-config';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
