9985bb8d87308818d011fbfe2be82b3cf53609b6
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 17:43:51 -0500

    Update Container.js
    following the demo

--- src/Container.js
+++ src/Container.js
@@ -1,13 +1,15 @@
-export default function Container({ children }) {
+const Container = ({ children }) => {
     return (
         <div style={containerStyle}>
             {children}
         </div>
-    )
-}
+    );
+};
 
 const containerStyle = {
     width: 900,
     margin: '0 auto',
     padding: '20px 0px'
-}
+};
+
+export default Container;
