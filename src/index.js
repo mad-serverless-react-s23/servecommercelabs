e0b07e0614cb84344d1d62f5d84891e80ca37646
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-27 23:49:41 -0500

    unnecessary
    getting rid of junk

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Router from './Router';
 import 'antd/dist/reset.css';
-import Amplify from 'aws-amplify';
-import config from './aws-config';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
 Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
 
