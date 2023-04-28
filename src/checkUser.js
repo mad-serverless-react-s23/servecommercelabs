355c3e457394b775f4204e213fdf771488285288
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 17:50:11 -0500

    Update checkUser.js
    commentary added for me

--- src/checkUser.js
+++ src/checkUser.js
@@ -1,21 +1,22 @@
-import { Auth } from 'aws-amplify';
+import { Auth } from 'aws-amplify'; // allows getting user info from cognito
 
 const checkUser = async(updateUser) => {
-    const userData = await Auth.currentSession()
+    const userData = await Auth.currentSession() // checking to see if we have data for this user
         .catch(err => console.log('You got a problem here: ', err)
         )
     if (!userData) {
         console.log('userData: ', userData)
         updateUser({})
-        return
+        return // returns if there is no user data
     }
-
+    // nested destructuring - idToken has info about user
     const { idToken: { payload }} = userData
+    // is this user in the Admin group? is it in any group?
     const isAuthorized = payload['cognito:groups'] &&
         payload['cognito:groups'].includes('Admin')
     updateUser({
         username: payload['cognito:username'],
-        isAuthorized
+        isAuthorized // if user is Admin, isAuthorized is set to true
     })
 }
 
