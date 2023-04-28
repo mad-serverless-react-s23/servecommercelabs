757ac96ebce9c6b8fe58859a95e53bff64064d14
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 18:22:34 -0500

    Update Nav.js
    following demo and adding my notes

--- src/Nav.js
+++ src/Nav.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Menu } from 'antd'
-import { HomeOutlined, UserOutlined, ProfileOutlined } from '@ant-design/icons'
-import { Hub } from 'aws-amplify'
-import checkUser from './checkUser'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+import { HomeOutlined, UserOutlined, ProfileOutlined } from '@ant-design/icons';
+import { Hub } from 'aws-amplify'; // listener for users sign in/out
+import checkUser from './checkUser';
 
-const Nav = (props) => {
-  const { current } = props
-  const [user, updateUser] = useState({})
+const Nav = ({ current }) => {
+  // const { current } = props if the only thing we need from 'props' is {current}
+  // destructure it and just use the bit you want as a parameter
+  const [user, updateUser] = useState({});
   useEffect(() => {
     checkUser(updateUser)
     Hub.listen('auth', (data) => {
-      const { payload: { event } } = data;
-      console.log('event: ', event)
+      const { payload: { event } } = data; // payload of the listener is the event
+      // event sign in/out, maybe others
+      console.log('event: ', event);
       if (event === 'signIn' || event === 'signOut') checkUser(updateUser)
+      // run the checkUser function whenever user signs in or out
     })
-  }, [])
+  }, []) // emptry array for only run when component loads
 
   return (
     <div>
@@ -31,7 +34,7 @@ const Nav = (props) => {
           </Link>
         </Menu.Item>
         {
-          user.isAuthorized && (
+          user.isAuthorized && ( // boolean short-circuit - shorter than if/else
             <Menu.Item key='admin'>
               <Link to='/admin'>
                 <ProfileOutlined />Admin
