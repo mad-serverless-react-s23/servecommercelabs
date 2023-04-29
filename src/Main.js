a319fef6ff3dca2898c20afa7acf67ad9fe7cb70
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-29 03:37:01 -0500

    Update Main.js
    following demo video

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import Container from './Container'
-import { API } from 'aws-amplify'
-import { List } from 'antd'
-import checkUser from './checkUser'
+import React, { useState, useEffect } from 'react';
+import Container from './Container';
+import { API } from 'aws-amplify';
+import { List } from 'antd';
+import checkUser from './checkUser';
 
-function Main() {
-  const [state, setState] = useState({products: [], loading: true})
-  const [user, updateUser] = useState({})
-  let didCancel = false
+const Main = () => {
+  const [state, setState] = useState({products: [], loading: true});
+  // ^^ state for the products to be displayed
+  const [user, updateUser] = useState({}); // set or update? book latter...
+  // ^^ state of the user object - admin or guest
+  let didCancel = false;
   useEffect(() => {
-    getProducts()
-    checkUser(updateUser)
-    return () => didCancel = true
+    getProducts() // brings in list of items
+    checkUser(updateUser) // check to be sure user is admin
+    return () => didCancel = true;
   }, [])
-  async function getProducts() {
-    const data = await API.get('servecommercelabsapi', '/products')
-    console.log('data: ', data)
+  const getProducts = async() => { // calls api, usually api calls are async
+    const data = await API.get('servecommercelabsapi', '/products');
+    console.log('data: ', data);
     if (didCancel) return
     setState({
       products: data.data.Items, loading: false
     })
   }
-  async function deleteItem(id) {
-    try {
+  const deleteItem = async(id) => {
+    try { // updates the view before calling api delete
       const products = state.products.filter(p => p.id !== id)
       setState({ ...state, products })
       await API.del('servecommercelabsapi', '/products', { body: { id } })
-      console.log('successfully deleted item')
+      console.log('Well, that item is gone.')
     } catch (err) {
       console.log('error: ', err)
     }
