8edbece0540c8c5cb3fae6a14402583d5883e350
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 19:20:14 -0500

    Update Router.js
    following the demo again... and taking notes

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import React, {useState, useEffect} from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import Nav from './Nav'
-import Admin from './Admin'
-import Main from './Main'
-import Profile from './Profile'
+import Nav from './Nav';
+import Admin from './Admin';
+import Main from './Main';
+import Profile from './Profile';
 
-export default function Router() {
-  const [current, setCurrent] = useState('home')
+const Router = () => {
+  const [current, setCurrent] = useState('home');
   useEffect(() => {
-    setRoute()
-    window.addEventListener('hashchange', setRoute)
-    return () =>  window.removeEventListener('hashchange', setRoute)
-  }, [])
-  function setRoute() {
-    const location = window.location.href.split('/')
-    const pathname = location[location.length-1]
-    console.log('pathname: ', pathname)
-    setCurrent(pathname ? pathname : 'home')
-  }
+    setRoute(); // function set below
+    window.addEventListener('hashchange', setRoute);
+    // ^^ listen for changes related to hash router
+    return () =>  window.removeEventListener('hashchange', setRoute);
+    // gives a function back after the setRoute function is called
+  }, []);
+  const setRoute = () => {
+    const location = window.location.href.split('/');
+    // splits the url parts of location into array of locations
+    const pathname = location[location.length - 1];
+    // the page we are on is last in the / array so this gets current page
+    console.log('The path we are traveling: ', pathname);
+    setCurrent(pathname ? pathname : 'home'); // set the available path or go home
+  };
   return (
     <HashRouter>
       <Nav current={current} />
@@ -29,5 +33,7 @@ export default function Router() {
         <Route element={<Main />} />
       </Routes>
     </HashRouter>
-  )
+  );
 }
+
+export default Router;
