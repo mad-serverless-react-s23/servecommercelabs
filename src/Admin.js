b040b1985fa39a23d2c48cf3b29e038d9675ca18
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-28 19:37:03 -0500

    Update Admin.js
    following demo to modernize a bit

--- src/Admin.js
+++ src/Admin.js
@@ -1,31 +1,31 @@
-import React, { useState } from 'react'
-import './App.css'
-import { Input, Button } from 'antd'
+import React, { useState } from 'react';
+import './App.css';
+import { Input, Button } from 'antd';
 
-import { API } from 'aws-amplify'
-import { withAuthenticator } from '@aws-amplify/ui-react'
+import { API } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 
 const initialState = {
   name: '', price: ''
-}
+};
 
-function Admin() {
-  const [itemInfo, updateItemInfo] = useState(initialState)
-  function updateForm(e) {
+const Admin = () => {
+  const [itemInfo, updateItemInfo] = useState(initialState);
+  const updateForm = (e) => {
     const formData = {
       ...itemInfo, [e.target.name]: e.target.value
-    }
-    updateItemInfo(formData)
-  }
-  async function addItem() {
+    };
+    updateItemInfo(formData);
+  };
+  const addItem = async() => {
     try {
       const data = {
         body: { ...itemInfo, price: parseInt(itemInfo.price) }
       }
-      updateItemInfo(initialState)
-      await API.post('servecommercelabsapi', '/products', data)
+      updateItemInfo(initialState);
+      await API.post('servecommercelabsapi', '/products', data);
     } catch (err) {
-      console.log('error adding item...')
+      console.err('We do not sell such things. ', err);
     }
   }
   return (
@@ -34,7 +34,7 @@ function Admin() {
         name='name'
         onChange={updateForm}
         value={itemInfo.name}
-        placeholder='Item name'
+        placeholder='What is this?'
         style={inputStyle}
       />
       <Input
@@ -42,18 +42,18 @@ function Admin() {
         onChange={updateForm}
         value={itemInfo.price}
         style={inputStyle}
-        placeholder='item price'
+        placeholder='How much is it?'
       />
       <Button
         style={buttonStyle}
         onClick={addItem}
-      >Add Product</Button>
+      >Put it on the digital shelf</Button>
     </div>
-  )
+  );
 }
 
 const containerStyle = { width: 400, margin: '20px auto' }
 const inputStyle = { marginTop: 10 }
 const buttonStyle = { marginTop: 10 }
 
-export default withAuthenticator(Admin)
+export default withAuthenticator(Admin);
